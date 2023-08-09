d2266d2e9887aac00bfd462de9930759e1dbe12a
eprayoga <endangprayoga95@gmail.com>
2023-08-09 13:46:09 +0700

    Delete console log

--- app/api/controllers/document.js
+++ app/api/controllers/document.js
@@ -130,7 +130,6 @@ module.exports = {
         try {
             const { id } = req.params;
             
-            console.log(id);
             pool.query("SELECT document_id, documents.created_by, document_name, signed_by, document_path, documents.created_at, fullname, email FROM documents INNER JOIN users ON documents.created_by=users.user_id WHERE document_id=$1", [id], (error, results) => {
                 if (error) throw error;
                 if (results.rows.length) {
